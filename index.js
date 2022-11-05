2ca68d14b3f44af4c5724267488a25dda6b158c5
maaza17 <maaz.haque17@gmail.com>
2022-11-06 02:46:56 +0500

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 require("dotenv").config();
 
 const userRoute = require('./routes/user.route')
@@ -7,6 +8,7 @@ const quizRoute = require('./routes/quiz.route')
 const attemptRoute = require('./routes/attempt.route')
 
 const app = express();
+app.use(helmet())
 
 app.use(express.urlencoded({ extended: true, limit: 20000000 }));
 app.use(express.json());
@@ -34,8 +36,10 @@ app.use(function(req, res, next) {
 app.get('/', (req, res) => {
     return res.status(200).json({
         author: "Maaz Ul Haque",
-        message: "This is a demo application for metaschool.so",
-        repository: "https://github.com/maaza17/quizApp"
+        description: "This is a demo application for metaschool.so",
+        repository: "https://github.com/maaza17/quizApp",
+        note: "Please download the postman collection hosted in the same repository & import into your postman client to interact with already formatted APIs. POST data is sent in the body in json format.",
+        linkToPostmanCollection: "https://github.com/maaza17/quizApp/blob/main/maazulhaque-quiz-app-postman-APIs.postman_collection.json"
     })
 })
 
